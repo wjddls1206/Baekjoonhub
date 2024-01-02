d221582277ea202f4f69cce2e014d70a3f6cb899
wjddls1206 <56677381+wjddls1206@users.noreply.github.com>
2024-01-03 03:24:37 +0900

    [Bronze III] Title: 저작권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a, i;
+	int result = 0;
+
+	cin >> a >> i;
+
+	result = a * (i - 1) + 1;
+
+	cout << result << endl;
+
+	return 0;
+}
