ceaff301eda8a752d3e8dd8566f10c28c96b9259
wjddls1206 <56677381+wjddls1206@users.noreply.github.com>
2024-01-03 03:21:22 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int A, B;
+	int T;
+
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+
+		cin >> A >> B;
+		cout << A + B << '\n';
+
+	}
+
+	return 0;
+}
