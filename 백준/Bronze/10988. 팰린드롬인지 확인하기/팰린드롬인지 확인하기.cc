6816d30ea9919be40e97f41a90bd5b4cfe2209dc
wjddls1206 <56677381+wjddls1206@users.noreply.github.com>
2024-01-03 03:12:51 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string s;
+	cin >> s;
+
+	for (int i = 0; i < s.length() / 2; i++) {
+
+		if (s[i] != s[s.length() - i - 1]) {
+
+			cout << 0;
+			return 0;
+
+		}
+
+	}
+
+	cout << 1;
+
+	return 0;
+}
