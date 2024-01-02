8abb966570f4e9805e156eec8581ab91b2be1fb4
wjddls1206 <56677381+wjddls1206@users.noreply.github.com>
2024-01-03 03:24:34 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a, b, c, d, e, f;
+
+	cin >> a >> b >> c >> d >> e >> f;
+
+	a = 1 - a;
+	b = 1 - b;
+	c = 2 - c;
+	d = 2 - d;
+	e = 2 - e;
+	f = 8 - f;
+
+	cout << a << " " << b << " " << c << " " << d << " " << e << " " << f << endl;
+
+	return 0;
+}
